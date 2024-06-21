a98a19f6a46f3f66ea11320a3acb2d9820e0dfc8
NArdraCloud78 <Neha@ardracloud.com>
2024-06-21 17:38:44 +0530

    add other photos  and badges   abouts  changes

--- ardraapp/src/Pages/AboutUs.js
+++ ardraapp/src/Pages/AboutUs.js
@@ -7,15 +7,16 @@ function AboutUs() {
     <>
     <div className="about-us-header">
       <div className="intro-section">
-        <span>Welcome to Our Company</span>
-        <h1>We are Innovators, Leaders, and Pioneers</h1>
+        <span>Welcome to Ardra.ai</span>
+        <h1>Join the future of energy with Ardra AI - Your partner in sustainable innovation.</h1>
       </div>
 
       <div className="about-section">
         <h2>About Us</h2>
         
         <p>
-          We are a forward-thinking company dedicated to driving innovation in our field. Our team of experts is committed to excellence and delivering outstanding results.
+          Ardra AI was started as a developer project in 2022. We were frustated by how difficult it was to build modern data pipelines that worked well across energy industry.
+          In November 2023, we launched a fully managed SaaS platform for facilities and utilities in the US market and joined Upekkha Accelerator's winter batch for US GTM. We're creating something special here, and we're excieted to build it with you.
         </p>
       </div>
       </div>
