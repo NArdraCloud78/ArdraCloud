abc1ddb11ee57355fbe49653cca55901d932af6b
NArdraCloud78 <Neha@ardracloud.com>
2024-06-21 17:02:33 +0530

    add photos and badges  abouts  changes

--- ardraapp/src/Pages/Home.js
+++ ardraapp/src/Pages/Home.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import './Home.css';
-// import star from '../Assist/homeassist/star.png';
-// import badge from '../Assist/homeassist/Badge.png';
+import star from '../Assist/homeassist/star.png';
+import badge from '../Assist/homeassist/Badge.png';
 import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <>
       <div className='headline mb-2'>
-        {/* <img width={80} height={50} src={star} alt="Star" /> */}
+        <img width={100} height={70} src={star} alt="Star" />
         <p>
           Manage Your Energy with ArdraAI: Unveiling
           <br />
           Insights, Empowering Decisions
         </p>
       </div>
-      {/* <div className='homebadge mb-3'>
-        <img width={200} height={100} src={badge} alt="Badge" />
-      </div> */}
+      <div className='homebadge mb-3'>
+        <img width={170} height={100} src={badge} alt="Badge" />
+      </div>
       <div className='bookdemo mb-3'>
-        <Link to="/demo">Book a demo</Link>
+        <Link to="https://calendly.com/pranjal-2diz/ardra-ai-socials">Book a demo</Link>
       </div>
     </>
   );
